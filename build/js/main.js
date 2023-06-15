5ae03f7dfd1cdacbf106cf8657f2c019078113d0
MarkoVoitovych <searchangeos@gmail.com>
2023-06-15 09:59:27 +0300

    basic types

---
+++ build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName = "Marko";
+let meaningOfLife;
+let isLoading;
+let album;
+meaningOfLife = 34;
+isLoading = true;
+album = true;
+album = null;
+const sum = (a, b) => {
+    return a + b;
+};
+sum(3, "3");
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+let myName: string = "Marko";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+meaningOfLife = 34;
+isLoading = true;
+album = true;
+album = null;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+sum(3, "3");
